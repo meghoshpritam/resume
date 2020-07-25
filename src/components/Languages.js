4d591cbe4dc0a951fb4558e80f0038936df11d7f
Pritam Ghosh <meghoshpritam@gmail.com>
2020-07-26 00:30:00 +0530

    :lipstick: language ui update

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -3,34 +3,81 @@ import style from './Languages.scss';
 
 const languages = [
 	{
-		from: { month: 'July', year: 2017 },
-		to: { month: 'July', year: 2020 },
-		institutionName: 'Kanchrapara College',
-		location: 'Kanchrapara, North 24-PGS',
-		under: 'University of Kalyani',
-		degree: 'Bachelor of Science',
-		specialization: 'Computer Science',
+		name: 'English',
+		description: 'Professional working proficiency',
+		read: true,
+		write: true,
+		speak: true,
 	},
 	{
-		from: { month: 'July', year: 2015 },
-		to: { month: 'July', year: 2017 },
-		institutionName: 'Majdia Rail Bazar High School',
-		location: 'Majdia, Nadia',
-		under: 'WBCHSE',
-		degree: 'Higher Secondary',
-		specialization: 'Science Stream',
+		name: 'Bengali',
+		description: 'Native or bilingual proficiency',
+		read: true,
+		write: true,
+		speak: true,
+	},
+	{
+		name: 'Hindi',
+		description: 'Limited working proficiency',
+		read: false,
+		write: false,
+		speak: true,
 	},
 ];
 
-const Language = ({}) => {
-	return <div className={style.language}>aaa</div>;
+const Language = ({ name, description, read, write, speak }) => {
+	return (
+		<div className={style.language}>
+			<div className={style.name}>{name}</div>
+			<div className={style.description}>{description}</div>
+			<div className={style.details}>
+				<div>
+					<span
+						className={
+							read
+								? `mdi mdi-check-circle ${style.done}`
+								: 'mdi mdi-check-circle-outline'
+						}
+					></span>{' '}
+					Read
+				</div>
+				<div>
+					<span
+						className={
+							write
+								? `mdi mdi-check-circle ${style.done}`
+								: 'mdi mdi-check-circle-outline'
+						}
+					></span>{' '}
+					Write
+				</div>
+				<div>
+					<span
+						className={
+							speak
+								? `mdi mdi-check-circle ${style.done}`
+								: 'mdi mdi-check-circle-outline'
+						}
+					></span>{' '}
+					Speak
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default () => {
 	return (
-		<div className={style.languageContainer}>
+		<div className={style.languagesContainer}>
 			{languages.map((language) => (
-				<Language />
+				<Language
+					key={language.name}
+					name={language.name}
+					description={language.description}
+					read={language.read}
+					write={language.write}
+					speak={language.speak}
+				/>
 			))}
 		</div>
 	);
