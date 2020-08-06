19c317a1b4172376be3b1c6182a87c77facd3eba
Pritam Ghosh <meghoshpritam@gmail.com>
2020-08-06 22:16:43 +0530

    :construction: update!

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,43 +1,66 @@
-import { h } from 'preact';
+import { useState, useEffect } from 'preact/hooks';
 import style from './Skills.scss';
 
-const Skill = ({ title, description, icon, url, progress, active }) => {
+const Skill = ({
+	title,
+	description,
+	icon,
+	url,
+	progress,
+	active,
+	open = false,
+	setOpen = () => {},
+}) => {
 	return (
-		<>
-			<div
-				className={style.skill}
-				// style={`--cursor: ${url !== '#' ? 'pointer' : 'default'}`}
-			>
+		<div className={style.skill}>
+			<div>
+				<div className={style.logo}>
+					<img src={icon} alt={`${title}-icon`} className={style.icon} />
+					{active ? <span className={style.active} /> : <span />}
+				</div>
 				<div>
-					<div className={style.logo}>
-						<img src={icon} alt={`${title}-icon`} className={style.icon} />
-						{active ? <span className={style.active} /> : <span />}
+					<div className={style.title}>
+						{url !== '#' ? (
+							<a href={url} target="_blank">
+								{title}
+							</a>
+						) : (
+							<> {title}</>
+						)}
 					</div>
-					<div>
-						<div className={style.title}>
-							{url !== '#' ? (
-								<a href={url} target="_blank">
-									{title}
-								</a>
-							) : (
-								<> {title}</>
-							)}
-						</div>
+					<div className={style.progressBarContainer}>
 						<div className={style.progressBar}>
 							<div style={{ width: `${progress}%` }}></div>
 						</div>
+						<span className="mdi mdi-chevron-down" onClick={setOpen} />
 					</div>
 				</div>
-				<div className={style.description}>{description}</div>
 			</div>
-		</>
+			<div
+				className={
+					open
+						? `${style.description} ${style.descriptionDialog}`
+						: `${style.description}`
+				}
+			>
+				{description}
+			</div>
+		</div>
 	);
 };
 
 export default ({ skills = [] }) => {
+	const [open, setOpen] = useState(-1);
+
+	useEffect(() => {
+		window.addEventListener('scroll', () => {
+			setOpen(-1);
+		});
+	}, []);
+
 	return (
 		<div className={style.skillsContainer}>
-			{skills.map((skill) => (
+			{skills.map((skill, idx) => (
 				<Skill
 					key={skill.title}
 					title={skill.title}
@@ -46,6 +69,11 @@ export default ({ skills = [] }) => {
 					url={skill.url}
 					progress={skill.progress}
 					active={skill.active}
+					open={idx === open}
+					setOpen={() => {
+						if (idx !== open) setOpen(idx);
+						else setOpen(-1);
+					}}
 				/>
 			))}
 		</div>
